0c5483c82c2c496258e4739fcdc2304b49c84bf6
garychen66 <rising.hunter@yandex.com>
2021-06-26 06:10:13 +0800

    fix #issue 15, 16: remove inappropriate usage of runblock, replace fragment with fragmentcontainerview

--- app/src/main/java/co/metalab/tech/interview/domain/GetEvolutions.kt
+++ app/src/main/java/co/metalab/tech/interview/domain/GetEvolutions.kt
@@ -3,11 +3,8 @@ package co.metalab.tech.interview.domain
 import co.metalab.tech.interview.data.Evolution
 import co.metalab.tech.interview.data.PokemonRepository
 import co.metalab.tech.interview.data.Result
-import kotlinx.coroutines.runBlocking
 
 class GetEvolutions {
 
-    suspend fun execute(pokeId: Int): Result<List<Evolution>> = runBlocking {
-        return@runBlocking PokemonRepository.getEvolutions(pokeId)
-    }
+    suspend fun execute(pokeId: Int): Result<List<Evolution>> = PokemonRepository.getEvolutions(pokeId)
 }
